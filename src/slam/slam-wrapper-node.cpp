5367ab07bfa92c24fc1bd34cac714be174bdd366
JussiKalliola <kalliolaj@gmail.com>
2024-03-19 14:39:52 +0200

    qos

--- src/slam/slam-wrapper-node.cpp
+++ src/slam/slam-wrapper-node.cpp
@@ -1,6 +1,7 @@
 #include "slam-wrapper-node.hpp"
 #include <cstdlib>
 #include <stdlib.h>
+#include <rmw/qos_profiles.h>
 
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
@@ -920,38 +921,38 @@ void SlamWrapperNode::CreatePublishers() {
     RCLCPP_INFO(this->get_logger(), "Creating a publisher for a topic /KeyFrame");
     keyframe_publisher_ = this->create_publisher<orbslam3_interfaces::msg::KeyFrame>(
         "/KeyFrame", 
-        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_best_available ));
+        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_sensor_data));
     
     /* MAP */
     RCLCPP_INFO(this->get_logger(), "Creating a publisher for a topic /Map");
     map_publisher_ = this->create_publisher<orbslam3_interfaces::msg::Map>(
         "/Map", 
-        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_best_available ));
+        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_sensor_data));
     
     /* MAPPOINT */
     RCLCPP_INFO(this->get_logger(), "Creating a publisher for a topic /MapPoint");
     map_point_publisher_ = this->create_publisher<orbslam3_interfaces::msg::MapPoint>(
         "/MapPoint", 
-        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_best_available ));
+        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_sensor_data));
     
     /* LocalMapping Active*/
     RCLCPP_INFO(this->get_logger(), "Creating a publisher for a topic /LocalMapping/Active");
     lm_active_publisher_ = this->create_publisher<orbslam3_interfaces::msg::Bool>(
         "/LocalMapping/Active", 
-        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_best_available ));
+        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_sensor_data));
 
     /* LocalMapping Active*/
     RCLCPP_INFO(this->get_logger(), "Creating a publisher for a topic /LocalMapping/Reset");
     lm_reset_requested_publisher_ = this->create_publisher<std_msgs::msg::Bool>(
         "/LocalMapping/Reset", 
-        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_best_available ));
+        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_sensor_data));
 
 
     /* Reset Active Map (System) */
     RCLCPP_INFO(this->get_logger(), "Creating a publisher for a topic /Map/Active/Reset");
     sys_reset_active_map_publisher_ = this->create_publisher<orbslam3_interfaces::msg::Int64>(
         "/Map/Reset/Active", 
-        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_best_available ));
+        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_sensor_data));
 
     /* Destroy */
     RCLCPP_INFO(this->get_logger(), "Creating a publisher for a topic /destroy");
@@ -982,21 +983,21 @@ void SlamWrapperNode::CreateSubscribers() {
     RCLCPP_INFO(this->get_logger(), "Creating a subscriber for a topic /KeyFrame");
     m_keyframe_subscriber_ = this->create_subscription<orbslam3_interfaces::msg::KeyFrame>(
         "KeyFrame",
-        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_best_available ),
+        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_sensor_data),
         std::bind(&SlamWrapperNode::GrabKeyFrame, this, std::placeholders::_1));
     
     /* Map */
     RCLCPP_INFO(this->get_logger(), "Creating a subscriber for a topic /Map");
     m_map_subscriber_ = this->create_subscription<orbslam3_interfaces::msg::Map>(
         "Map",
-        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_best_available ),
+        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_sensor_data),
         std::bind(&SlamWrapperNode::GrabMap, this, std::placeholders::_1));
 
     /* MapPoint */
     RCLCPP_INFO(this->get_logger(), "Creating a subscriber for a topic /MapPoint");
     m_map_point_subscriber_ = this->create_subscription<orbslam3_interfaces::msg::MapPoint>(
         "MapPoint",
-        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_best_available ),
+        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_sensor_data),
         std::bind(&SlamWrapperNode::GrabMapPoint, this, std::placeholders::_1));
     
 
@@ -1004,21 +1005,21 @@ void SlamWrapperNode::CreateSubscribers() {
     RCLCPP_INFO(this->get_logger(), "Creating a subscriber for a topic /LocalMapping/Active");
     m_lm_active_subscriber_ = this->create_subscription<orbslam3_interfaces::msg::Bool>(
         "LocalMapping/Active",
-        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_best_available ),
+        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_sensor_data),
         std::bind(&SlamWrapperNode::GrabLMActive, this, std::placeholders::_1));
 
     /* LocalMapping reset requested */
     RCLCPP_INFO(this->get_logger(), "Creating a subscriber for a topic /LocalMapping/Reset");
     m_lm_reset_requested_subscriber_ = this->create_subscription<std_msgs::msg::Bool>(
         "LocalMapping/Reset",
-        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_best_available ),
+        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_sensor_data),
         std::bind(&SlamWrapperNode::GrabLMResetRequested, this, std::placeholders::_1));
 
     /* LocalMapping reset requested */
     RCLCPP_INFO(this->get_logger(), "Creating a subscriber for a topic /Map/Reset/Active");
     m_sys_reset_active_map_subscriber_ = this->create_subscription<orbslam3_interfaces::msg::Int64>(
         "Map/Reset/Active",
-        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_best_available ),
+        rclcpp::QoS(rclcpp::KeepLast(10), rmw_qos_profile_sensor_data),
         std::bind(&SlamWrapperNode::GrabResetActiveMap, this, std::placeholders::_1));
     
 
